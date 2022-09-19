dbed02bf7968da81b602ffa402d3781e685c8f41
aloh333 <al3190589@gmail.com>
2022-09-20 00:46:39 +0900

    first commit

---
+++ helloworld/data.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    char a = 0x12, b = 0x34;
+    short c = 0x5678;
+    int t;
+    void *p = (int *)&t;
+    *(short *)p = c;
+    *(char *)(p + 2) = b;
+    *(char *)(p + 3) = a;
+
+    printf("%X\n", t);
+
+    return 0;
+}
